baea7327f110611f8166f4e80c3882d16167407c
Joe Guilmette <joe@soflyy.com>
2024-02-15 14:38:39 +0700

    move author tag to plugin header

--- import-users-from-csv.php
+++ import-users-from-csv.php
@@ -1,31 +1,13 @@
 <?php
 /*
 Plugin Name: Import Users from CSV
-Plugin URI: http://wordpress.org/extend/plugins/import-users-from-csv/
+Plugin URI: https://wordpress.org/plugins/import-users-from-csv/
 Description: Import Users data and metadata from a csv file.
-Version: 1.1
-Author: Andrew Lima
-Author URI: https://andrewlima.co.za
+Version: 1.2
+Author: WP All Import
+Author URI: https://www.wpallimport.com/
 License: GPL2
 Text Domain: import-users-from-csv
-*/
-
-/*
- * Copyright 2011  Ulrich Sossou  (https://github.com/sorich87)
- * Copyright 2018  Andrew Lima  (https://github.com/andrewlimaza/import-users-from-csv)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 /**
--- readme.txt
+++ readme.txt
@@ -6,8 +6,6 @@ Requires PHP: 7.0
 Tested up to: 6.4.2
 Stable tag: 1.2
 License: GPLv2 or later
-Author: WP All Import
-Author URI: https://www.wpallimport.com/
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 
